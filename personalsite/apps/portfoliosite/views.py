a07db8f94fa7ebaabe72ade45d8b38c7f7a892cc
jekrimo <jekrimo@gmail.com>
2016-08-22 15:07:10 -0600

    adds

--- personalsite/apps/portfoliosite/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render, redirect
-
-def index(request):
-    return render(request, "portfoliosite/index.html")
-
-def show(request):
-    return render(request, "portfoliosite/show.html")
